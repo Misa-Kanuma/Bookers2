3b2a3961051dc925fe5393d286beec50404fb953
Misa-Kanuma <misa.kanuma@gmail.com>
2020-06-20 18:09:33 +0900

    [Add] index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
-  # def show
-  # 	@user = User.find(params[:id])
-  # end
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		@user.user_id = current_user.id
+		@user.save
+		redirect_to_user_path
+	end
+
+   def index
+   	@users = User.all
+   	@user = User.find(params[:id])
+   end
+
+   def show
+    @users = User.all
+   	@user = User.find(params[:id])
+   end
+
+   def edit
+   	@user = User.find(params[:id])
+   end
+
+private
+	def user_params
+		params.require(:user).permit(:name, :profile_image, :introduction)
+	end
 end
