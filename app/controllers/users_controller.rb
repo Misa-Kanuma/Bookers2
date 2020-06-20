24adb2252c570751ce1a33c67ccfffcb94f3f834
Misa-Kanuma <misa.kanuma@gmail.com>
2020-06-20 23:49:42 +0900

    [Add] Top

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		@user.save
+		redirect_to users_path
+	end
+
    def index
    	@users = User.all
    	@user = current_user
@@ -13,6 +23,13 @@ class UsersController < ApplicationController
    def edit
    	@user = User.find(params[:id])
    end
+
+   def update
+   	@user = User.find(params[:id])
+   	@user.update(user_params)
+   	redirect_to users_path(user.id)
+   end
+
 
 private
 	def user_params
