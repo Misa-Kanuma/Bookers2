66f909f1df856cf7ace7025e86e937e8990f78d3
Misa-Kanuma <misa.kanuma@gmail.com>
2020-06-23 21:19:11 +0900

    [Finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,43 +4,42 @@ class UsersController < ApplicationController
   end
 
 	def create
-		@user = User.new(user_params)
-		if @user.save
-		      redirect_to users_path
-    else
-          render :new
-    end
+		  @user = User.new(user_params)
+		  if @user.save
+		     redirect_to users_path
+      else
+         render :new
+      end
 	end
 
-   def index
-   	@users = User.all
-   	@user = User.find_by(id: current_user.id)
-    @newbook = Book.new
-   end
+  def index
+   	  @users = User.all
+   	  @user = User.find_by(id: current_user.id)
+      @newbook = Book.new
+  end
 
    def show
-    @users = User.all
-    @user = User.find(params[:id])
-    @books = @user.books
-    @newbook = Book.new
+      @users = User.all
+      @user = User.find(params[:id])
+      @books = @user.books
+      @newbook = Book.new
    end
 
    def edit
-   	@user = User.find(params[:id])
-    if @user != current_user
+   	  @user = User.find(params[:id])
+      if @user != current_user
       redirect_to user_path(current_user)
-    end
+      end
    end
 
    def update
-   	@user = User.find(params[:id])
-   	if @user.update(user_params)
-   	    redirect_to user_path(@user.id), notice: "You have updated book successfully."
-    else
-        render 'edit'
-    end
+       @user = User.find(params[:id])
+   	   if @user.update(user_params)
+          redirect_to user_path(@user.id), notice: "You have updated book successfully."
+       else
+          render 'edit'
+       end
    end
-
 
 private
 	def user_params
