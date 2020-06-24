de42f7af4917d817505ebf94e56dff4d2e9ee3b4
vagrant <vagrant@localhost.localdomain>
2020-06-24 07:51:44 +0000

    [Update] migrate修修正

--- db/migrate/20200623084745_homes.rb
+++
@@ -1,5 +0,0 @@
-class Homes < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :homes
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_084745) do
+ActiveRecord::Schema.define(version: 2020_06_20_091946) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
